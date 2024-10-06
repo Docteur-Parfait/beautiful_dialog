9d7daaeefd95270adc6bc4958d1599c75a7e8ad6
Armel Bogue <komlanbogue@gmail.com>
2024-10-06 23:45:23 +0000

    "Added new dialog method showNotifDialog to DialogClass, updated DialogView with new button and dialog, and modified pubspec.lock file."

--- lib/dialogs/dialog_class.dart
+++ lib/dialogs/dialog_class.dart
@@ -14,6 +14,76 @@ import '../widgets/expanding_dialog.dart';
 
 class DialogClass {
   // add your dialog here as a static method
+
+
+static void showNotifDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          contentPadding: const EdgeInsets.all(16.0),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              SizedBox(
+                height: 100,
+                child: Image.network(
+                  'https://cdn-icons-png.flaticon.com/512/5337/5337153.png',
+                  fit: BoxFit.contain,
+                  errorBuilder: (context, error, stackTrace) {
+                    return const Icon(
+                      Icons.error_outline,
+                      color: Colors.red,
+                      size: 50,
+                    );
+                  },
+                ),
+              ),
+              const SizedBox(height: 16.0),
+              const Text(
+                'Get a heads up on everything Venmo',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 8.0),
+              const Text(
+                'Find out when you get paid or receive payment requests. Get updates on new features, discounts, and promos.',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 14),
+              ),
+              const SizedBox(height: 8.0),
+              const Text(
+                'You can change this at any time in Settings.',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 14),
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Not now'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                // Action pour activer les notifications
+              },
+              child: const Text('Allow notifications'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   static void showYesNoConfirmationDialog(
     BuildContext context, {
     required String title,
--- lib/views/dialog_view.dart
+++ lib/views/dialog_view.dart
@@ -66,12 +66,12 @@ class _DialogViewState extends State<DialogView> {
           child: Wrap(
             children: [
               CustomButton(
-                text: "yes or No confirmation",
+                text: "Yes or No confirmation",
                 author: "Tech Pastor",
                 onTap: () => DialogClass.showYesNoConfirmationDialog(
                   context,
                   title: "Confirm",
-                  subtitle: "Do you want to confirm",
+                  subtitle: "Do you want to confirm?",
                 ),
               ),
               CustomButton(
@@ -80,7 +80,7 @@ class _DialogViewState extends State<DialogView> {
                 onTap: () => DialogClass.showOkConfirmationDialog(
                   context,
                   title: "Confirm",
-                  message: "Action confirm successfully",
+                  message: "Action confirmed successfully",
                 ),
               ),
               CustomButton(
@@ -88,7 +88,7 @@ class _DialogViewState extends State<DialogView> {
                 author: "Tech Pastor",
                 onTap: () => DialogClass.showDangerAlertDialog(
                   context,
-                  warningMessage: "Do you want to logout?",
+                  warningMessage: "Do you want to log out?",
                 ),
               ),
               CustomButton(
@@ -105,7 +105,7 @@ class _DialogViewState extends State<DialogView> {
                 author: "littleDarkBug",
                 onTap: () => DialogClass.showExpandingDialog(
                   context,
-                  message: "Try to close it at first try.",
+                  message: "Try to close it on the first try.",
                 ),
               ),
               CustomButton(
@@ -114,39 +114,91 @@ class _DialogViewState extends State<DialogView> {
                 onTap: () => DialogClass.showWarningAlertDialog(
                   context,
                   warningMessage:
-                      "Are you sure you want to delete this post? this action\ncannot be undone.",
+                      "Are you sure you want to delete this post? This action\ncannot be undone.",
                 ),
               ),
               CustomButton(
-
-                text: "Warning alert",
-                author: "gotflo",
-                onTap: () => DialogClass.showWarningAlertDialog(
-                  context,
-                  warningMessage:
-                      "Are you sure you want to delete this post? this action\ncannot be undone.",
-                ),
-              ),
-              CustomButton(
-                text: "Swhith theme alert",
+                text: "Switch theme alert",
                 author: "Lecodeur",
-                onTap: () => DialogClass.showSwitchThemeDialog(
-                  context,
-                ),
+                onTap: () => DialogClass.showSwitchThemeDialog(context),
               ),
               CustomButton(
                 text: "Error Alert",
                 author: "shubhanshu-02",
                 onTap: () => DialogClass.showErrorDialog(
                   context,
-                  message: "An error occurred while processing your request."
+                  message: "An error occurred while processing your request.",
                 ),
               ),
-
+              CustomButton(
+                text: "Show notification Dialog",
+                author: "Armel Bogue",
+                onTap: () => showVenmoDialog(context),
+              ),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  // Méthode pour afficher la boîte de dialogue "Notification"
+  void showVenmoDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          contentPadding: const EdgeInsets.all(16.0),
+          content: const Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Icon(
+                Icons.notifications_active,
+                size: 50,
+                color: Colors.blueAccent,
+              ),
+              SizedBox(height: 16.0),
+              Text(
+                'Get a heads up on everything',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 8.0),
+              Text(
+                'Find out when you get paid or receive payment requests. Get updates on new features, discounts, and promos.',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 14),
+              ),
+              SizedBox(height: 8.0),
+              Text(
+                'You can change this at any time in Settings.',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 14),
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Not now'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                // Action pour activer les notifications
+              },
+              child: const Text('Allow notifications'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
