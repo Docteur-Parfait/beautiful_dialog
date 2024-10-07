aebdb9993e8b38ef77e7662bdc70576fe3016fc3
Didi Orlog SOSSOU <didisossou@gmail.com>
2024-10-07 10:06:49 +0000

    (feat) : Implt using Gesture Detector ans Slight refactor

--- lib/widgets/expanding_dialog.dart
+++ lib/widgets/expanding_dialog.dart
@@ -38,6 +38,16 @@ class ExpandingDialogState extends State<ExpandingDialog>
     _controller.reverse();
   }
 
+  void _onUpdate([DragUpdateDetails? details]) {
+    if (_controller.status == AnimationStatus.dismissed) {
+      _rotationY = -0.1; // Small rotation
+      _controller.forward();
+    }else {
+      _rotationY = 0.0;
+      _controller.reverse();
+    }
+  }
+
   @override
   void dispose() {
     _controller.dispose();
@@ -46,7 +56,8 @@ class ExpandingDialogState extends State<ExpandingDialog>
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
+    return
+      AnimatedBuilder(
       animation: _controller,
       builder: (context, child) {
         return Transform(
@@ -60,76 +71,81 @@ class ExpandingDialogState extends State<ExpandingDialog>
               side: const BorderSide(
                   color: Colors.black, style: BorderStyle.solid, width: 4),
             ),
-            child: MouseRegion(
-              cursor: SystemMouseCursors.click,
-              onEnter: _onEnter,
-              onExit: _onExit,
-              child: Container(
-                decoration: BoxDecoration(
-                  gradient: const LinearGradient(
-                    colors: [Color(0xff878787), Color(0xffe0e0e0)],
-                    stops: [0.25, 0.75],
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
+            child:
+            GestureDetector(
+              onPanUpdate: _onUpdate,
+              onTap: _onUpdate,
+              child:MouseRegion(
+                cursor: SystemMouseCursors.click,
+                onEnter: _onEnter,
+                onExit: _onExit,
+                child: Container(
+                  decoration: BoxDecoration(
+                    gradient: const LinearGradient(
+                      colors: [Color(0xff878787), Color(0xffe0e0e0)],
+                      stops: [0.25, 0.75],
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                    ),
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                padding: const EdgeInsets.only(
-                    top: 20.0, bottom: 0, left: 20, right: 20),
-                child: Padding(
                   padding: const EdgeInsets.only(
-                      top: 20.0, bottom: 0, left: 25, right: 25),
-                  child: SizedBox(
-                    width: 280,
-                    height: 180,
-                    child: Column(
-                      children: [
-                        const Align(
-                          alignment: Alignment.topLeft,
-                          child: Text(
-                            "You got it!",
-                            style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.w600,
-                                color: Colors.black),
-                          ),
-                        ),
-                        const Padding(padding: EdgeInsets.only(bottom: 30)),
-                        Text(
-                          widget.message,
-                          textAlign: TextAlign.center,
-                          style: const TextStyle(
-                              fontSize: 14, color: Colors.black87),
-                        ),
-                        const Padding(padding: EdgeInsets.only(bottom: 30)),
-                        ElevatedButton(
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                          style: ElevatedButton.styleFrom(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 40, vertical: 15),
-                            backgroundColor: Colors.black,
-                            elevation: 0,
-                            side: const BorderSide(
-                                color: Colors.white24,
-                                style: BorderStyle.solid,
-                                width: 1),
-                          ).copyWith(
-                            shape: WidgetStateProperty.all<
-                                RoundedRectangleBorder>(
-                              RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(5),
-                              ),
+                      top: 20.0, bottom: 0, left: 20, right: 20),
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        top: 20.0, bottom: 0, left: 25, right: 25),
+                    child: SizedBox(
+                      width: 280,
+                      height: 180,
+                      child: Column(
+                        children: [
+                          const Align(
+                            alignment: Alignment.topLeft,
+                            child: Text(
+                              "You got it!",
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.black),
                             ),
                           ),
-                          child: const Text(
-                            "Done",
+                          const Padding(padding: EdgeInsets.only(bottom: 30)),
+                          Text(
+                            widget.message,
                             textAlign: TextAlign.center,
-                            style: TextStyle(fontSize: 14, color: Colors.white),
+                            style: const TextStyle(
+                                fontSize: 14, color: Colors.black87),
                           ),
-                        ),
-                      ],
+                          const Padding(padding: EdgeInsets.only(bottom: 30)),
+                          ElevatedButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            style: ElevatedButton.styleFrom(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 40, vertical: 15),
+                              backgroundColor: Colors.black,
+                              elevation: 0,
+                              side: const BorderSide(
+                                  color: Colors.white24,
+                                  style: BorderStyle.solid,
+                                  width: 1),
+                            ).copyWith(
+                              shape: WidgetStateProperty.all<
+                                  RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(5),
+                                ),
+                              ),
+                            ),
+                            child: const Text(
+                              "Done",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(fontSize: 14, color: Colors.white),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
