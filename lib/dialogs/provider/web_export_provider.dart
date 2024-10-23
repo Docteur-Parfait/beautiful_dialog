2190fb05ec7c8d86cec9832509b0490f92549f0a
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-10-23 17:06:12 +0000

    Refactor data_visualization_dialog.dart and add export functionality for charts on different platforms

---
+++ lib/dialogs/provider/web_export_provider.dart
@@ -0,0 +1,17 @@
+import 'dart:html' as html;
+import 'dart:typed_data';
+
+Future<void> exportBytes({
+  required Uint8List bytes,
+  required String fileName,
+  required String mimeType,
+}) async {
+  final blob = html.Blob([bytes], mimeType);
+  final url = html.Url.createObjectUrlFromBlob(blob);
+
+  final anchor = html.AnchorElement(href: url)
+    ..setAttribute('download', fileName)
+    ..click();
+
+  html.Url.revokeObjectUrl(url);
+}
