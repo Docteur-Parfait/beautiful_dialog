56b2449aae32a4827835a13973cceb4cb3f51434
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-10-23 17:40:51 +0000

    Refactor web_export_provider.dart to add support for exporting bytes on the web

--- lib/dialogs/provider/web_export_provider.dart
+++ lib/dialogs/provider/web_export_provider.dart
@@ -1,11 +1,26 @@
-import 'dart:html' as html;
 import 'dart:typed_data';
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'dart:html' as html;
 
 Future<void> exportBytes({
   required Uint8List bytes,
   required String fileName,
   required String mimeType,
 }) async {
+  if (kIsWeb) {
+    _exportBytesWeb(bytes: bytes, fileName: fileName, mimeType: mimeType);
+  } else {
+    throw UnsupportedError('exportBytes is only supported on the web');
+  }
+}
+
+void _exportBytesWeb({
+  required Uint8List bytes,
+  required String fileName,
+  required String mimeType,
+}) {
+  // Import 'dart:html' only if running in a web environment
+
   final blob = html.Blob([bytes], mimeType);
   final url = html.Url.createObjectUrlFromBlob(blob);
 
